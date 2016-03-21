db0a7a8d9386f65f5c38e9756911442aa21abdca
orel <orel@orel-toledano>
2016-03-21 10:45:33 +0200

    finished+testers

--- M1/Test/Test.java
+++ M1/Test/Test.java
@@ -5,15 +5,35 @@ public class Test {
 
 	@org.junit.Test
 	//test1
-	public void test(){
-	
-		Graph G = new Graph("C:\\Res\\tinyEWG.txt","C:\\Res\\test1.txt","C:\\Res\\Out.txt");
-
-		
-		
-		//boolean check = Graph.CMP_FILES("C:\\Res\\Solution_test3.txt_mediumEWD.txt_1458310286550_.txt","C:\\Res\\Out.txt");
-		
-		//assertTrue(check);
+	public void test1(){
+		Graph G = new Graph("C:\\testers\\test1\\graph1.txt","C:\\testers\\test1\\black1.txt","C:\\testers\\test1\\Out1.txt");
+		boolean check = Graph.CMP_FILES("C:\\testers\\test1\\out1.txt","C:\\testers\\test1\\Out1.txt");
+		assertTrue(check);
+	}
+	public void test2(){
+		Graph G = new Graph("C:\\testers\\test2\\graph2.txt","C:\\testers\\test2\\black2.txt","C:\\testers\\test2\\Out2.txt");
+		boolean check = Graph.CMP_FILES("C:\\testers\\test2\\out2.txt","C:\\testers\\test2\\Out2.txt");
+		assertTrue(check);
+	}
+	public void test3(){
+		Graph G = new Graph("C:\\testers\\test3\\graph3.txt","C:\\testers\\test3\\black3.txt","C:\\testers\\test3\\Out3.txt");
+		boolean check = Graph.CMP_FILES("C:\\testers\\test3\\out3.txt","C:\\testers\\test3\\Out3.txt");
+		assertTrue(check);
+	}
+	public void test4(){
+		Graph G = new Graph("C:\\testers\\test4\\graph4.txt","C:\\testers\\test4\\black4.txt","C:\\testers\\test4\\Out4.txt");
+		boolean check = Graph.CMP_FILES("C:\\testers\\test4\\out4.txt","C:\\testers\\test4\\Out4.txt");
+		assertTrue(check);
+	}
+	public void test5(){
+		Graph G = new Graph("C:\\testers\\test5\\graph5.txt","C:\\testers\\test5\\black5.txt","C:\\testers\\test5\\Out5.txt");
+		boolean check = Graph.CMP_FILES("C:\\testers\\test5\\out5.txt","C:\\testers\\test5\\Out5.txt");
+		assertTrue(check);
+	}
+	public void test6(){
+		Graph G = new Graph("C:\\testers\\test6\\graph6.txt","C:\\testers\\test6\\black6.txt","C:\\testers\\test6\\Out6.txt");
+		boolean check = Graph.CMP_FILES("C:\\testers\\test6\\out6.txt","C:\\testers\\test6\\Out6.txt");
+		assertTrue(check);
 	}
 
 }
